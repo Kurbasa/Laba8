28dff30e8a5448b153127b57bf115ac684ca0b26
Kurbasa <taras.nosko@oa.edu.ua>
2024-04-01 09:34:55 +0300

    Feat: Final project

--- nuxt-app/Pages/StudentList.vue
+++ nuxt-app/Pages/StudentList.vue
@@ -1,5 +1,33 @@
 <script setup lang="ts">
-const people = [{
+import { ref, computed } from 'vue'
+
+const columns = [{
+  key: 'id',
+  label: 'ID'
+}, {
+  key: 'title',
+  label: 'Title'
+}, {
+  key: 'description',
+  label: 'Description'
+}, {
+  key: 'price',
+  label: 'Price'
+}, {
+  key: 'rating',
+  label: 'Rating'
+},{
+  key: 'brand',
+  label: 'Brand'
+}, {
+  key: 'category',
+  label: 'Category'
+}, {
+  key: 'thumbnail',
+  label: 'Thumbnail'
+}]
+
+const people = ref([{
   id: 1,
   name: 'Lindsay Walton',
   title: 'Front-end Developer',
@@ -35,80 +63,58 @@ const people = [{
   title: 'Principal Designer',
   email: 'floyd.miles@example.com',
   role: 'Member'
-}, {
-  id: 7,
-  name: 'Emily Selman',
-  title: 'VP, User Experience',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 8,
-  name: 'Kristin Watson',
-  title: 'VP, Human Resources',
-  email: '',
-  role: 'Member'
-}, {
-  id: 9,
-  name: 'Emma Watson',
-  title: 'Front-end Developer',
-  email: '',
-  role: 'Member'
-}, {
-  id: 10,
-  name: 'John Doe',
-  title: 'Designer',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 11,
-  name: 'Jane Doe',
-  title: 'Director of Product',
-  email: '',
-  role: 'Member'
-}, {
-  id: 12,
-  name: 'John Smith',
-  title: 'Copywriter',
-  email: '',
-  role: 'Admin'
-}, {
-  id: 13,
-  name: 'Jane Smith',
-  title: 'Senior Designer',
-  email: '',
-  role: 'Owner'
-}]
+}])
 
-const page = ref(1)
-const pageCount = 5
-const q = ref('')
-const filteredRows = computed(() => {
+
+const { data } = await useFetch<any>('https://dummyjson.com/products');
+let productslist = data.value.products;
+
+const q = ref('');
+const page = ref(1);
+const pageCount = 5;
+const pagemem = 1;
+
+const filteredData = computed(() => {
   if (!q.value) {
-    return people.slice((page.value - 1) * pageCount, (page.value) * pageCount)
+    return productslist;
   }
 
-  return people.filter((person) => {
-    return Object.values(person).some((value) => {
-      return String(value).toLowerCase().includes(q.value.toLowerCase())
-    })
-  })
-const rows = computed(() => {
+  return productslist.filter((products) => {
+    page.value = 1;
+    return Object.values(products).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase());
+    });
+  });
+});
 
-})
-})
+const rows = computed(() => {
+  return filteredData.value.slice((page.value - 1) * pageCount, page.value * pageCount);
+});
+
 </script>
 
 <template>
-  <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
-    <UInput v-model="q" placeholder="Filter people..." />
-  </div>
   <div>
-    <UTable :rows="filteredRows" />
+    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+      <UInput v-model="q" placeholder="Filter people..." />
+    </div>
+    <UTable :rows="rows" :columns="columns">
+      <template #thumbnail-data="{row}">
+        <img :src="row.thumbnail" alt="" class="w-[100px] h-[100px]">
+      </template>
+      <template #description-data="{row}">
+        <div class="text-wrap">{{ row.description }}</div>
+      </template>
+      <!-- Rating cell -->
+      <template #rating-data="{row}">
+        <div :class="{ 'bg-red-500/40': row.rating < 4.5, 'bg-green-500/40': row.rating >= 4.5 }" class="px-3 py-2">
+          {{ row.rating }}
+        </div>
+      </template>
 
-    <div class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
-      <UPagination v-model="page" :page-count="pageCount" :total="people.length" />
+    </UTable>
+    <div class="flex justify-center px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
+      <UPagination v-model="page" :page-count="pageCount" :total="productslist.length" />
     </div>
   </div>
-
 </template>
-
